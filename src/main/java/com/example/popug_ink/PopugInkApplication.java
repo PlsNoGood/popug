097e7696048026462b74bc85df29e471cf68e85c
Pls <iamnotpls@gmail.com>
2024-02-23 20:26:43 +0300

    Initial commit

---
+++ src/main/java/com/example/popug_ink/PopugInkApplication.java
@@ -0,0 +1,13 @@
+package com.example.popug_ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopugInkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopugInkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/popug_ink/PopugInkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popug_ink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopugInkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
